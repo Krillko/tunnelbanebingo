5fba73b6e20ae00c90926a7df52cb9d4b4f1f35b
Kristoffer Ekendahl <kristoffer.ekendahl@iveo.se>
2026-04-22 22:29:35 +0200

    Demo page on github pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+  app: {
+    baseURL: process.env.NUXT_APP_BASE_URL ?? '/',
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxt/eslint'],
   ssr: false,
