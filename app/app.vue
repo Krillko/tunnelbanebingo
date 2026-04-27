2a026a8d6fd37fedbf20513f41510fce0696353c
Kristoffer Ekendahl <kristoffer.ekendahl@iveo.se>
2026-04-27 12:49:14 +0200

    Fix favicon

--- app/app.vue
+++ app/app.vue
@@ -2,6 +2,15 @@
 useSeoMeta({
   ogType: 'website',
 });
+
+useHead({
+  link: [
+    { rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico' },
+    { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+    { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+  ],
+});
 </script>
 
 <template>
